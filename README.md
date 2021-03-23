9c7a45f4023b68b3995ceee607083bc7a7e62a10
digitune <digitune+github@gmail.com>
2021-03-23 20:26:35 +0900

    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # docker-puppeteer
 Dockerfile for puppeteer and so on.
+
+# これは何？
+
+docker上でpuppeteerを使うためのDockerfileの一例とそちらを使ってweb pageのscreenshotを取るためのsh scriptです。
+
+基本的には[こちら](https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-in-docker)にある公式ドキュメントを参考にしました。
+
+お楽しみいただければ幸いです。
---
+++ takeScreenshot.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+if [ $# -lt 2 ]; then
+  echo "usage: $0 [URL] [FILENAME]"
+  exit 1
+fi
+
+js_script=`cat <<EOT
+const puppeteer = require('puppeteer');
+
+// constants
+const DL_PATH='/home/node/Downloads';
+const COOKIES=DL_PATH + '/cookies.json';
+
+async function scrollToBottom(page, viewportHeight) {
+  const getScrollHeight = () => {
+    return Promise.resolve(document.documentElement.scrollHeight)
+  };
+
+  let scrollHeight = await page.evaluate(getScrollHeight);
+  let currentPosition = 0;
+
+  while (currentPosition < scrollHeight) {
+
+    const nextPosition = currentPosition + viewportHeight;
+
+    await page.evaluate(function (scrollTo) {
+        return Promise.resolve(window.scrollTo(0, scrollTo))
+      }, nextPosition);
+
+    currentPosition = nextPosition;
+    console.log("currentPosition: " + currentPosition);
+
+    // stop reevaluating scrollHeight when scrolling down
+    //scrollHeight = await page.evaluate(getScrollHeight);
+    console.log("scrollHeight: " + scrollHeight);
+  }
+}
+
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: 'google-chrome-stable',
+    userDataDir: '/home/node/.config/google-chrome',
+    headless: true,
+    args: [
+      '--lang=ja,en-US,en',
+      '--disable-dev-shm-usage',
+      '--unhandled-rejections=strict'
+    ]
+  });
+  const viewportHeight = 768;
+  const page = await browser.newPage();
+  await page.setViewport({width: 1024, height: viewportHeight, deviceScaleFactor: 1.5});
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'ja,en-US;q=0.8,en;q=0.6'
+  });
+
+  // set URL
+  const url = '${1}';
+  await page.goto(url, {waitUntil: ['load', 'networkidle2'], timeout: 60000})
+    .catch(e => console.log('timeout exceed. proceed to next operation'));
+
+  await scrollToBottom(page, viewportHeight);
+  await page.screenshot({path: DL_PATH + '/${2}', type: 'jpeg', fullPage: true});
+
+  console.log("save screenshot: " + url + " to ${2}");
+  await browser.close();
+})();
+EOT
+`
+
+docker run -i --init --rm --cap-add=SYS_ADMIN \
+    -v ${HOME}/var/docker/docker-puppeteer/home:/home/node \
+    --name puppeteer-chrome puppeteer-chrome-linux \
+    node -e "${js_script}"
+
+# backup
+#    xvfb-run -a --server-args="-screen 0 1280x800x24 -ac -nolisten tcp -dpi 96 +extension RANDR" \
